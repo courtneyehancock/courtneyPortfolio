9cdaaa2a202bd951975519c862d88ba2361f97e6
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-01-28 08:22:16 -0500

    Add main content section

--- page.php
+++ page.php
@@ -1,24 +1,26 @@
 <?php get_header(); ?>
 
-<div class="container">
-    <div class="row">
-      <div class="nine columns">
-        <?php
-          if(have_posts()){
-            while(have_posts()){
-              the_post();?>
+<div class="main-content">
+  <div class="container">
+      <div class="row">
+        <div class="nine columns">
+          <?php
+            if(have_posts()){
+              while(have_posts()){
+                the_post();?>
 
-              <h2><?php the_title(); ?></h2>
-              <?php the_content();
+                <h2><?php the_title(); ?></h2>
+                <?php the_content();
+              }
             }
-          }
-        ?>
-      </div>
-      <div class="three columns">
-        <?php get_sidebar(); ?>
+          ?>
+        </div>
+        <div class="three columns">
+          <?php get_sidebar(); ?>
 
+        </div>
       </div>
-    </div>
+  </div>
 </div>
 
 <?php get_footer(); ?>
