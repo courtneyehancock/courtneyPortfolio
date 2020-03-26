39c6b5731755232efdd211b8590cb7ba4ea25d4f
Hancock <courtneyehanco1k-dce@ssrb-vpn2-8-199.vpn.ufl.edu>
2020-03-26 12:39:06 -0400

    Adjust footer columns

--- footer.php
+++ footer.php
@@ -1,17 +1,17 @@
 <footer>
   <div class="container">
     <div class="row">
-      <div class="col-md-5">
+      <div class="col-md-3">
         <!--Logo/text widget-->
         <?php dynamic_sidebar('left-footer'); ?>
       </div>
 
-      <div class="col-md-1">
+      <div class="col-md-4">
         <!--Middle Left Space-->
         <?php dynamic_sidebar('middle-left-footer'); ?>
       </div>
 
-      <div class="col-md-3">
+      <div class="col-md-2">
         <!--Menu Title Widget-->
         <?php dynamic_sidebar('menu-title-footer'); ?>
         <!-- Navigation -->
